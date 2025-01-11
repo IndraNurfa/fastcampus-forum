d9fee7f3dca9e45c0ecb8f58935abfb148700d46
Ahmad Indra Nurfauzi <ahmad.nurfauzi@moladin.com>
2025-01-11 20:25:37 +0700

    Create Post

--- cmd/main.go
+++ cmd/main.go
@@ -5,8 +5,11 @@ import (
 
 	"github.com/IndraNurfa/fastcampus/internal/configs"
 	"github.com/IndraNurfa/fastcampus/internal/handlers/memberships"
+	"github.com/IndraNurfa/fastcampus/internal/handlers/posts"
 	membershipRepo "github.com/IndraNurfa/fastcampus/internal/repository/memberships"
+	postRepo "github.com/IndraNurfa/fastcampus/internal/repository/posts"
 	membershipSvc "github.com/IndraNurfa/fastcampus/internal/service/memberships"
+	postSvc "github.com/IndraNurfa/fastcampus/internal/service/posts"
 	"github.com/IndraNurfa/fastcampus/pkg/internalsql"
 	"github.com/gin-gonic/gin"
 )
@@ -37,11 +40,20 @@ func main() {
 		log.Fatal("Gagal koneksi ke database", err)
 	}
 
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+
 	membershipRepo := membershipRepo.NewRepository(db)
+	postRepo := postRepo.NewRepository(db)
+
 	membershipService := membershipSvc.NewService(cfg, membershipRepo)
+	postService := postSvc.NewService(cfg, postRepo)
 
 	membershipsHandler := memberships.NewHandler(r, membershipService)
 	membershipsHandler.RegisterRoutes()
+
+	postHandler := posts.NewHandler(r, postService)
+	postHandler.RegisterRoutes()
 
 	r.Run(cfg.Service.Port) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
